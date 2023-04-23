3403e685df9606ffcaa8a41491c5d63008207985
DaFlopper <131584995+DaFlopper@users.noreply.github.com>
2023-04-23 12:41:41 +0200

    Final Update

--- Extension.js
+++ Extension.js
@@ -8,16 +8,64 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    // Define the 'math' block
-    ext.math = function(num1, num2, callback) {
+    // Define the 'addition' block
+    ext.addition = function(num1, num2, callback) {
         var result = parseFloat(num1) + parseFloat(num2);
         callback(result);
+    };
+    
+    // Define the 'subtraction' block
+    ext.subtraction = function(num1, num2, callback) {
+        var result = parseFloat(num1) - parseFloat(num2);
+        callback(result);
+    };
+    
+    // Define the 'multiplication' block
+    ext.multiplication = function(num1, num2, callback) {
+        var result = parseFloat(num1) * parseFloat(num2);
+        callback(result);
+    };
+    
+    // Define the 'division' block
+    ext.division = function(num1, num2, callback) {
+        var result = parseFloat(num1) / parseFloat(num2);
+        callback(result);
+    };
+    
+    // Define the 'numb1 > numb2' block
+    ext.numb1_greater_than_numb2 = function(num1, num2) {
+        return parseFloat(num1) > parseFloat(num2);
+    };
+    
+    // Define the 'numb1 < numb2' block
+    ext.numb1_less_than_numb2 = function(num1, num2) {
+        return parseFloat(num1) < parseFloat(num2);
+    };
+    
+    // Define the 'equal' block
+    ext.equal = function(val1, val2) {
+        return parseFloat(val1) === parseFloat(val2);
+    };
+    
+    // Define the 'add to equals' block
+    ext.add_to_equals = function(num1, num2, eq, callback) {
+        var result = parseFloat(num1) + parseFloat(num2);
+        if (result === parseFloat(eq)) {
+            callback();
+        }
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', '%n + %n', 'math', 1, 1],
+            ['R', '%n + %n', 'addition', 1, 1],
+            ['R', '%n - %n', 'subtraction', 1, 1],
+            ['R', '%n * %n', 'multiplication', 3, 6],
+            ['R', '%n / %n', 'division', 3, 6],
+            ['b', '%n > %n', 'numb1_greater_than_numb2', 3, 6],
+            ['b', '%n < %n', 'numb1_less_than_numb2', 3, 6],
+            ['', '%n = %n', 'equal', 3, 3],
+            ['w', 'add %n to %n = %n', 'add_to_equals', 1, 1, 2]
         ],
         menus: {},
         url: 'https://github.com/DaFlopper/Scratch-Math/blob/main/README.md'
