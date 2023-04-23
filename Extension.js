2276b3d0d10daaad81b80342befea5cd1edefc7f
DaFlopper <131584995+DaFlopper@users.noreply.github.com>
2023-04-23 11:15:58 +0200

    Create Extension.js

---
+++ Extension.js
@@ -0,0 +1,28 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+    
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Define the 'math' block
+    ext.math = function(num1, num2, callback) {
+        var result = parseFloat(num1) + parseFloat(num2);
+        callback(result);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', '%n + %n', 'math', 1, 1],
+        ],
+        menus: {},
+        url: 'https://example.com/scratch-extension/'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Math Extension', descriptor, ext);
+})({});
